970ffacf36410d2b1496470d96e40da0898c584c
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-08-24 14:16:52 -0600

    Update create user to include name, add 5 users in seeding

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,15 +1,15 @@
 import db from "../client.js";
 import bcrypt from "bcrypt";
 
-export const createUser = async ({ username, password }) => {
+export const createUser = async ({ name, username, password }) => {
   const securePassword = await bcrypt.hash(password, 10);
   const sql = `
-      INSERT INTO users (username, password)
-      VALUES ($1, $2)
+      INSERT INTO users (name, username, password)
+      VALUES ($1, $2, $3)
       RETURNING id, username
    `;
 
-  const { rows: [user] } = await db.query(sql, [username, securePassword]);
+  const { rows: [user] } = await db.query(sql, [name, username, securePassword]);
   console.log(user);
   return user;
 };
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,17 @@
 import db from './client.js';
+import { createUser } from './queries/users.js';
+
+const seed = async () => {
+   for (let i = 1; i <=5; i++) {
+      const newUser = {
+         name: `Testy${i}`,
+         username: `Test${i}`,
+         password: `password`
+      }
+      await createUser(newUser);
+   }
+}
+
+await db.connect();
+await seed();
+await db.end();
